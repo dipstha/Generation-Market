fd5820fa71102cf4bb6d13522dc78e729b3b7667
Keith <ketjr81@gmail.com>
2022-09-09 15:36:23 -0700

    updated fetch, so it connects to backend.

--- JS/itemsController.js
+++ JS/itemsController.js
@@ -1,35 +1,65 @@
-
-
 class ProductsController {
-    constructor(currentId = 0){
-        this.products = [];
-        this.currentId = currentId;
-    }
+  constructor(currentId = 0) {
+    this.products = [];
+    this.currentId = currentId;
+  }
 
-    addItem(name, description, img, createdAt){
-    
-        const product = {
-            id: this.currentId++,
-            name: name,
-            description: description,
-            img: img,
-            createdAt: createdAt
-        };
+  addItem(name, description, img, createdAt) {
+    const product = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      img: img,
+      createdAt: createdAt,
+    };
+    this.products.push(product);
+
+    localStorage.setItem("products", JSON.stringify(this.products));
+
+    this.uploadItem({ name, description, img, createdAt });
+  }
+
+  save({ name, description, img, createdAt }) {
+    const data = { name, description, img, createdAt };
+
+    fetch(`http://localhost:8080/api/items/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Success:`, data);
+      })
+      .catch((error) => {
+        console.log(`Error:`, error);
+      });
+  };
+
+  update({name, description, img, createdAt}) {
+        //TODO
+  };
+
+  delete(id) {
+    //TODO
+  };
+
+  findById(id) {
+    //TODO
+  }
+
+  productsFromLocalStorage() {
+    const storageproducts = localStorage.getItem("products");
+    if (storageproducts) {
+      const products = JSON.parse(storageproducts);
+      for (let i = 0; i < products.length; i++) {
+        const product = products[i];
         this.products.push(product);
-
-       localStorage.setItem("products", JSON.stringify(this.products));
+      }
     }
-   
-    productsFromLocalStorage() {
-        const storageproducts = localStorage.getItem("products")
-        if (storageproducts) {
-            const products = JSON.parse(storageproducts)
-            for (let i = 0; i < products.length; i++) {
-                const product = products[i];
-                this.products.push(product);
-            }
-        }
-    }
+  }
 }
 
 // const items = new ProductsController();
@@ -40,6 +70,4 @@ class ProductsController {
 // , '../images/bowls.jpg', '2021-12-20');
 // console.log(items);
 
-
- export { ProductsController };
-
+export { ProductsController };
